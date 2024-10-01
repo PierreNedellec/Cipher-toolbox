ebc9302195466a87f67760af5b87ce9ba9f7eccc
PierreNedellec <pierrethebear888@gmail.com>
2024-10-01 21:06:56 +0100

    Finished the wordfreq function

--- main_code.py
+++ main_code.py
@@ -2,8 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import string
+import matplotlib.pyplot as plt
+import numpy as np
 
 # VARIABLES
+
+corpus = open('brown_corpus_words.txt','r').read()
+
 global letters
 letters = list(string.ascii_uppercase)
 
@@ -23,7 +28,7 @@ def char_freq(text):
 
     for character in text:
         alphabet[character] += 1
-
+        
     for freq in range(max(alphabet.values()),0,-1):
         for place in list(string.printable):
             if alphabet[place] == freq:
@@ -103,36 +108,47 @@ def monoalphabeticdecrypt(text,key):
     
 def formatcorpus(file):
     corpus = open(file,'r').read()
-    print(corpus)
     
     pun = string.punctuation
     pun = list(pun)
-    print(pun)
-    print(len(pun))
-    print(corpus)
+    
     for item in pun:
         if item == '-' or item == "'":
-            print('skipped')
-            continue
-        
+            continue        
         corpus = corpus.replace(item,'')
-        print(corpus)
-    print(corpus)
         
     corpus = corpus.replace("""'s""",'')
     corpus = corpus.replace("""s'""",'s')
     corpus = corpus.replace('\n',' ')
+    corpus = corpus.replace('- ',' ')
+    corpus = corpus.repalce(' -',' ')
     while '  ' in corpus:
         corpus = corpus.replace('  ',' ')
     corpus = corpus.upper()
     print(corpus)
     return corpus
 
+def wordfreq(text):
+    words = dict()
+    text = text.split(' ')
+    
+    for word in text:
+        
+        if word in words:
+            words[word] += 1
+            if word == 'HAT':
+                print(words)
+        else:
+            words[word] = 1
+    
+    return words
+
 # CODE
 
-cipher = """AREIR JKZVJ JJKPY RJVZL JKEAZ VKKUE KVNEJ LBGCB MVBGR ADPPC LIGYE VZKUE KNRNC LYASV BAEBP KUVBT CSBCK RVBKU RMCYL ZRKUE KPCLJ RBKZR PCLEI RIVTU KKUEK VKUEJ JCZRM EYLRE JEJFR GVEYR AVKVC BCSFC RJNCI XDLKV GCLYA BCKJR REBPK UVBTI RZEIX EDYRE DCLKV KKUEK ZVTUK ROFYE VBKUR FIRJR BGRCI KURAR EKUCS KURPC LBTZE BPCLS CLBAV BKURY VDIEI PVKVJ ESVBR JFRGV ZRBEB AVKNE JNVKU JCZRI RYLGK EBGRK UEKVE KKEGX RAKUR DVBAV BTEJV AVABC KROFR GKKCS VBAEB PKUVB TIRZE IXEDY RKURI RVBPC LGEBV ZETVB RZPJL IFIVJ RKURB KCAVJ GCMRI KUREK KEGUR ABCKR GYREI YPNIV KKRBV BEGVF URIEB AGEIR SLYYP GCBGR EYRAD RKNRR BKURY REKUR IEBAK URRBA DCEIA JRMRB KURBV EJJLZ RAKUV JKCDR EFLDY VJURI JKIVG XKCFI CZCKR KURRA VKVCB TVMRB KURFI CZVBR BGRCS RBGIP FKVCB VBKUR KEYRD LKGCB JLYKV BTNVK UZPSI VRBAJ VBKUR DCCXK IEARB CBRCS KURZU EAURE IACSJ LGUEG CBGRV KEBAV GCLYA BCKSV BAEBP IRGCI ACSEB EAMRI KVJVB TGEZF EVTBI RSRII VBTKC JLGUE GLIVC JVKPE KKUVJ FCVBK VSRYK VJUCL YAEKY REJKE KKRZF KKCAR GVFUR IKURZ RJJET REBAH LVGXY PAVJG CMRIR AKUEK KUREL KUCIU EALJR AKURM RIPZV YAGCB MRBKV CBCSE JUVSK GVFUR IGYRE IYPVB KRBAR AKCDR DICXR BKURS VIJKY VBRIR EAIRG RBKRM RBKJU EMRZE ARVKG YREIR IKUEB RMRIK CZRKU EKVEZ VBZCI KEYAE BTRIE BAKUR IRJKG CBKEV BRAZL GUKCE YEIZZ RRJFR GVEYY PKURI EKURI GYLZJ PIRMR IJREG ICJKV GNUVG UIREA ZLIAR IKUCL TUSCI KURYC BTRJK KVZRV EJJLZ RAVKK CIREA IRAIL ZNUVG UGELJ RAZRZ LGUGC BSLJV CBNUR KURIC IBCKK UVJGC BGREY RAZRJ JETRF CVBKJ KCKUR LBSCI KLBEK RAREK UCSPC LIVBK RIYCF RIVJB CKEFF EIRBK KCZRD LKVKU VBXPC LEIRI VTUKK UEKVK JFIRJ RBGRJ UCLYA DRJLS SVGVR BKKCR BGCLI ETRKU RFCYV GRKCI RCFRB KURGE JRVNV YYSCI NEIAE GCFPC SKUVJ YRKKR IKCTR KURIN VKUZP SVBAV BTJKC ZPSIV RBAKC ZUEIF RIEJG UVRSC SFCYV GRURJ UCLYA UEMRK URFCN RIEBA VBSYL RBGRK CACJC NVKUZ PDRJK NVJUR JZELI VGRAI ZELIV GRNUV KRZA"""
+cipher = """text"""
+
+v = wordfreq(corpus[:1000000])
 
 
-f = open('brown_corpusv5.txt','w')
-f.write(formatcorpus('brown_corpusv8.txt'))
+
 
