e444c3ae383232c4ef6997d81efe3a9e7372f83f
PierreNedellec <pierrethebear888@gmail.com>
2024-10-04 22:43:00 +0100

    Changed frequency of the quadragrams to represent proportion of text. Changed the logarithms so base is e.

--- calculation.py
+++ calculation.py
@@ -12,11 +12,11 @@ def quadragrams():
     if lettersonly:
         #text = formatcorpus(text)
         text = text.replace('-','')
-        text = text.replace(' ','')
+        #text = text.replace(' ','')
         text = text.replace('\n','')
     
     quadragrams = dict()
-    letters = string.ascii_uppercase
+    letters = string.ascii_uppercase+' '
     keys = []
     for a in letters:
         for b in letters:
@@ -28,18 +28,15 @@ def quadragrams():
         quadragrams[key] = 0
     
     
-    for pos in range(len(text)-4):
+    for pos in range(len(text)-3):
         item = text[pos:pos+4]
-        if text[pos:pos+4] in quadragrams:
-            quadragrams[text[pos:pos+4]] += 1
-        else:
-            print('ERROR')
-            print(item)
-            print(pos)
-            print(len(text))
-            break
+        if item in quadragrams:
+            quadragrams[item] += 1
+        
+    for k,v in quadragrams.items():
+        quadragrams[k] = v/(len(text)-3)
             
-    g = open('english_quadragrams_frequencies.txt','w')
+    g = open('english_quadragrams_frequencies_spaces.txt','w')
     new = ''
     for k,v in quadragrams.items():
         new += (k+';'+str(v)+'\n')
@@ -60,16 +57,19 @@ def englishquadragrams(spaces = False):
     return quaddict
 
 
-original = englishquadragrams(1)
-new = ''
-
-for k,v in original.items():
-    v = int(v)
-    if v ==0:
-        v = -1
-    else:
-        v = log(v,10)
-    new += (k+';'+str(v)+'\n')  
+def addlogs():
+    original = englishquadragrams(0)
+    new = ''
     
-g = open('english_quadragram_frequencies_spaces_logvalues.txt','w')
-g.write(new)
+    for k,v in original.items():
+        v = float(v)
+        if v ==0:
+            v = -17
+        else:
+            v = log(v)
+        new += (k+';'+str(v)+'\n')  
+        
+    g = open('english_quadragram_frequencies_logvalues.txt','w')
+    g.write(new)
+    
+addlogs()
